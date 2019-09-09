f36ac5e948e6a579cbdc16cde700385d307a7c3e
Levi barbosa <levibarbos4@hotmai.com>
2019-09-09 16:42:36 -0300

    adicionada a função bubble

--- bubble.c
+++ bubble.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h> // apagar essa linha para compilar no linux
+
+#define MAX 3
+
+void bubble(int *vetor[]);
 
 int main(int argc, char const *argv[])
 {
-    int vetor[15];
+    setlocale(LC_ALL, "Portuguese");
+    int vetor[MAX];
 
+    for (int i = 0; i < MAX; i++)
+    {
+        printf("Digite um número: ");
+        scanf("%i", &vetor[i]);
+    }
+
+    system("pause"); // apagar essa linha para compilar no linux
     return 0;
 }
 
+void bubble(int *vetor[])  // ordena o vetor usando busca binária 
+{
+    int aux;
+    for (int i = 0; i < MAX; i++)
+    {
+        if (vetor[i] < vetor[i + 1])
+        {
+            aux = vetor[i];
+            vetor[i] = vetor[i + 1];
+            vetor[i + 1] = aux;
+        }
+    }
+}
