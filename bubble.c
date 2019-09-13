1131d6fc7ca78a1068637911d3606dc6349e93ac
Levi barbosa <levibarbos4@hotmai.com>
2019-09-13 01:28:32 -0300

    atividade bubble e pesquisa binária

--- bubble.c
+++ bubble.c
@@ -2,31 +2,47 @@
 #include <stdlib.h>
 #include <locale.h> // apagar essa linha para compilar no linux
 
-#define MAX 4
+#define MAX 5
 
 void bubble(int vetor[]);
+int binary_sort(int vetor[], int chave, int n);
 
 int main(int argc, char const *argv[])
 {
     setlocale(LC_ALL, "Portuguese");
-    int vetor[MAX] = {1, 3, 5, 2};
+    int vetor[MAX], num;
 
+    puts("Digite 5 números inteiros: ");
+    for (int i = 0; i < MAX; i++)
+    {
+        printf("Digite: ");
+        scanf("%i", &vetor[i]);
+    }
+    
+    puts("Vetor não ordenado: ");
     for (int i = 0; i < MAX; i++)
         printf("%i ", vetor[i]);
 
     bubble(vetor);
-    puts("");
+    puts("\nVetor ordenado: ");
 
     for (int i = 0; i < MAX; i++)
         printf("%i ", vetor[i]);
 
-    puts("");
+    puts("\nAgora digite um número para pesquisar no vetor: ");
+    printf("Número: ");
+    scanf("%i", &num);
+
+    if (binary_sort(vetor, num, MAX) == 0)
+        puts("Elemento não encontrado!");
+    else
+        puts("Elemento encontrado!");
 
     system("pause"); // apagar essa linha para compilar no linux
     return 0;
 }
 
-void bubble(int vetor[MAX]) // ordena o vetor usando busca binÃ¡ria
+void bubble(int vetor[MAX]) // ordena o vetor usando busca binária
 {
     int aux;
     for (int contador = 1; contador < MAX; contador++)
@@ -41,4 +57,22 @@ void bubble(int vetor[MAX]) // ordena o vetor usando busca binÃ¡ria
             }
         }
     }
+}
+
+int binary_sort(int vetor[], int chave, int n)
+{
+    int inf, sup, meio;
+    inf = 0;
+    sup = n - 1;
+    while (inf <= sup)
+    {
+        meio = (inf + sup) / 2;
+        if (chave == vetor[meio])
+            return meio;
+        else if (chave < vetor[meio])
+            sup = meio - 1;
+        else
+            inf = meio + 1;
+    }
+    return 0; /* não encontrado */
 }
