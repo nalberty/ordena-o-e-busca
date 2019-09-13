20b833432558c880077281c82e7cd64c523641c5
Levi barbosa <levibarbos4@hotmai.com>
2019-09-13 01:12:06 -0300

    correção de erros, inserida a ordenação por bubble

--- bubble.c
+++ bubble.c
@@ -2,35 +2,43 @@
 #include <stdlib.h>
 #include <locale.h> // apagar essa linha para compilar no linux
 
-#define MAX 3
+#define MAX 4
 
-void bubble(int *vetor[]);
+void bubble(int vetor[]);
 
 int main(int argc, char const *argv[])
 {
     setlocale(LC_ALL, "Portuguese");
-    int vetor[MAX];
+    int vetor[MAX] = {1, 3, 5, 2};
 
     for (int i = 0; i < MAX; i++)
-    {
-        printf("Digite um número: ");
-        scanf("%i", &vetor[i]);
-    }
+        printf("%i ", vetor[i]);
+
+    bubble(vetor);
+    puts("");
+
+    for (int i = 0; i < MAX; i++)
+        printf("%i ", vetor[i]);
+
+    puts("");
 
     system("pause"); // apagar essa linha para compilar no linux
     return 0;
 }
 
-void bubble(int *vetor[])  // ordena o vetor usando busca binária 
+void bubble(int vetor[MAX]) // ordena o vetor usando busca binária
 {
     int aux;
-    for (int i = 0; i < MAX; i++)
+    for (int contador = 1; contador < MAX; contador++)
     {
-        if (vetor[i] < vetor[i + 1])
+        for (int i = 0; i < MAX - 1; i++)
         {
-            aux = vetor[i];
-            vetor[i] = vetor[i + 1];
-            vetor[i + 1] = aux;
+            if (vetor[i] > vetor[i + 1])
+            {
+                aux = vetor[i];
+                vetor[i] = vetor[i + 1];
+                vetor[i + 1] = aux;
+            }
         }
     }
 }
